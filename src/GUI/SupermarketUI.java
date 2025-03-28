9e490d1803cc2f37e5cff6e4f632f847f5581b8d
hv353 <sunlightcorleone@gmai.com>
2025-03-28 22:21:16 +0700

    suaQLSach

--- src/GUI/SupermarketUI.java
+++ src/GUI/SupermarketUI.java
@@ -2,12 +2,10 @@ package GUI;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
-import javax.swing.table.DefaultTableModel;
 
 import java.awt.*;
 
 public class SupermarketUI {
-    private static DefaultTableModel cartModel;
     private static CardLayout cardLayout;
     private static JPanel mainPanel;
     private static JButton activeButton = null;
@@ -78,191 +76,211 @@ public class SupermarketUI {
     	    // Tạo panel "Bán Hàng"
     	    JPanel banHangPanel = new JPanel();
     	    banHangPanel.add(new JLabel("Bán Hàng"));
-    	    banHangPanel.setLayout(new BorderLayout());
-    	    JPanel taoHoaDon = new JPanel();
-    	    taoHoaDon.setLayout(null);
-    	    banHangPanel.add(taoHoaDon,BorderLayout.NORTH);
-    	    JLabel headTaoHD= new JLabel("TẠO HÓA ĐƠN BÁN HÀNG");
-    	    headTaoHD.setForeground(Color.RED);
-    	    headTaoHD.setBounds(250, 0, 250, 100);
-    	    JLabel maHD= new JLabel("Mã HD");
-    	    maHD.setBounds(25, 100, 50, 10);
-    	    JLabel maNV= new JLabel("Mã NV");
-    	    maNV.setBounds(200, 100, 50, 10);
-    	    JLabel maKH= new JLabel("Mã KH");
-    	    maKH.setBounds(400, 100, 50, 10);
-    	    JLabel maGG= new JLabel("Mã GG");
-    	    maGG.setBounds(600, 100, 50, 10);
-    	    JLabel ngayLap= new JLabel("Ngày lập");
-    	    ngayLap.setBounds(25, 160, 50, 20);
-    	    JTextField txtMaHD= new JTextField();
-    	    txtMaHD.setBounds(70, 95, 80, 20);
-    	    JTextField txtMaNV= new JTextField();
-    	    txtMaNV.setBounds(250, 95, 80, 20);
-    	    JTextField txtMaKH= new JTextField();
-    	    txtMaKH.setBounds(450, 95, 80, 20);
-    	    JTextField txtMaGG= new JTextField();
-    	    txtMaGG.setBounds(650, 95, 80, 20);
-    	    JTextField txtNgayLap= new JTextField();
-    	    txtNgayLap.setBounds(80, 160, 100, 20);
-    	    
-    	    
-    	    taoHoaDon.add(headTaoHD);
-    	    taoHoaDon.add(maHD);
-    	    taoHoaDon.add(txtMaHD);
-    	    taoHoaDon.add(maNV);
-    	    taoHoaDon.add(txtMaNV);
-    	    taoHoaDon.add(maKH);
-    	    taoHoaDon.add(txtMaKH);
-    	    taoHoaDon.add(maGG);
-    	    taoHoaDon.add(txtMaGG);
-    	    taoHoaDon.add(ngayLap);
-    	    taoHoaDon.add(txtNgayLap);
-    	    
-    	    banHangPanel.add(taoHoaDon);
+    
     	    
     	    // Tạo panel "Quản Lý Sách"
-    	    JPanel quanLySachPanel = new JPanel();
-    	    quanLySachPanel.add(new JLabel());
-    	    JPanel quanLySach = new JPanel(new GridLayout(4, 1,2,20));
+    	    JPanel quanLySachPanel = new JPanel(new BorderLayout());
+			quanLySachPanel.setPreferredSize(new Dimension(0,600));
+    	    // JPanel quanLySach = new JPanel(new BorderLayout());
+			
+			// quanLySachPanel.add(quanLySach);
     	    JPanel	QLS1 = new JPanel();
     	    JLabel titelQLS = new JLabel ("QUẢN LÝ SÁCH");
+			Font font = new Font("Arial", Font.BOLD, 26);
+			Font font1 = new Font("Arial", Font.BOLD, 16);
+			titelQLS.setFont(font);
     	    titelQLS.setForeground(Color.RED);
-    	    titelQLS.setBounds(250, 0, 250, 100);
+    	    titelQLS.setBounds(0, 0, 250, 100);
     	    QLS1.add(titelQLS);
-    	    JPanel QLS2= new JPanel(new GridLayout(1,4,30,2));
-    	    JPanel txt_QLS2= new JPanel(new GridLayout(8,2));
-    	    JLabel maSach = new JLabel ("mã sách: ");
-    	    JLabel maNXB = new JLabel ("mã NXB: ");
-    	    JLabel maTheLoai = new JLabel ("mã thể loại: ");
-    	    JLabel maTacGia = new JLabel ("mã tác giả: ");
-    	    JLabel tenSach = new JLabel ("tên sách: ");
-    	    JLabel namXuatBan = new JLabel ("năm xuất bản: ");
-    	    JLabel soLuong = new JLabel ("số lượng: ");
-    	    JLabel donGia = new JLabel ("đơn giá: ");
-    	    JTextField txtMaSach = new JTextField();
-    	    JTextField txtMaNXB = new JTextField();
-    	    JTextField txtMaTheLoai = new JTextField();
-    	    JTextField txtMaTacGia = new JTextField();
-    	    JTextField txtTenSach = new JTextField();
-    	    JTextField txtNamXuatBan = new JTextField();
-    	    JTextField txtSoLuong = new JTextField();
-    	    JTextField txtDonGia = new JTextField();
+			quanLySachPanel.add(QLS1,BorderLayout.NORTH);
+			JPanel quanLySachCenter = new JPanel(new GridLayout(3,1)); 
+    		JPanel QLS2= new JPanel(new GridLayout(1,4,30,2));
+			QLS2.setMaximumSize(new Dimension(450,600));
+			//JPanel QLS2= new JPanel();
+			//QLS2.setLayout(new BoxLayout(QLS2, BoxLayout.X_AXIS));
+			//  JPanel QLS2= new JPanel(new GridBagLayout());
+			//  GridBagConstraints gbc = new GridBagConstraints();
+
+			//  gbc.insets = new Insets(5, 5, 5, 5);
+			//  gbc.weightx = 1; // Chiếm 30% không gian ngang
+			//  gbc.fill = GridBagConstraints.HORIZONTAL; // Căn chỉnh ngang
+			//  gbc.gridx = 0; gbc.gridy = 0;
+			
+	
+
+    	    JPanel txt_QLS2= new JPanel(new GridLayout(8,2, 0 ,5));
+    	     JLabel maSach = new JLabel ("mã sách: ");
+			 maSach.setFont(font1);
+    	     JLabel maNXB = new JLabel ("mã NXB: ");
+			 maNXB.setFont(font1);
+    	     JLabel maTheLoai = new JLabel ("mã thể loại: ");
+			 maTheLoai.setFont(font1);
+    	     JLabel maTacGia = new JLabel ("mã tác giả: ");
+			 maTacGia.setFont(font1);
+    	     JLabel tenSach = new JLabel ("tên sách: ");
+			 tenSach.setFont(font1);
+    	     JLabel namXuatBan = new JLabel ("năm xuất bản: ");
+			 namXuatBan.setFont(font1);
+    	     JLabel soLuong = new JLabel ("số lượng: ");
+			 soLuong.setFont(font1);
+    	     JLabel donGia = new JLabel ("đơn giá: ");
+			 donGia.setFont(font1);
+    	     JTextField txtMaSach = new JTextField();
+    	     JTextField txtMaNXB = new JTextField();
+    	     JTextField txtMaTheLoai = new JTextField();
+    	     JTextField txtMaTacGia = new JTextField();
+    	     JTextField txtTenSach = new JTextField();
+    	     JTextField txtNamXuatBan = new JTextField();
+    	     JTextField txtSoLuong = new JTextField();
+    	     JTextField txtDonGia = new JTextField();
     	    
-    	    txt_QLS2.add(maSach);
-    	    txt_QLS2.add(txtMaSach);
-    	    txt_QLS2.add(maNXB);
-    	    txt_QLS2.add(txtMaNXB);
-    	    txt_QLS2.add(maTheLoai);
-    	    txt_QLS2.add(txtMaTheLoai);
-    	    txt_QLS2.add(maTacGia);
-    	    txt_QLS2.add(txtMaTacGia);
-    	    txt_QLS2.add(tenSach);
-    	    txt_QLS2.add(txtTenSach);
-    	    txt_QLS2.add(namXuatBan);
-    	    txt_QLS2.add(txtNamXuatBan);
-    	    txt_QLS2.add(soLuong);
-    	    txt_QLS2.add(txtSoLuong);
-    	    txt_QLS2.add(donGia);
-    	    txt_QLS2.add(txtDonGia);
-    	    QLS2.add(txt_QLS2);
+    	     txt_QLS2.add(maSach);
+    	     txt_QLS2.add(txtMaSach);
+    	     txt_QLS2.add(maNXB);
+    	     txt_QLS2.add(txtMaNXB);
+    	     txt_QLS2.add(maTheLoai);
+    	     txt_QLS2.add(txtMaTheLoai);
+    	     txt_QLS2.add(maTacGia);
+    	     txt_QLS2.add(txtMaTacGia);
+    	     txt_QLS2.add(tenSach);
+    	     txt_QLS2.add(txtTenSach);
+    	     txt_QLS2.add(namXuatBan);
+    	     txt_QLS2.add(txtNamXuatBan);
+    	     txt_QLS2.add(soLuong);
+    	     txt_QLS2.add(txtSoLuong);
+    	     txt_QLS2.add(donGia);
+    	     txt_QLS2.add(txtDonGia);
+    	     QLS2.add(txt_QLS2);
     	    
-    	    JPanel btn_QLS2 = new JPanel( new GridLayout(4,1,2,10));
-    	    JButton btnThem= new JButton("Thêm");
-    	    btnThem.setForeground(Color.WHITE);
-    	    btnThem.setBackground(Color.CYAN);
-    	    JButton btnSua= new JButton("Sửa");
-    	    btnSua.setForeground(Color.WHITE);
-    	    btnSua.setBackground(Color.blue);
-    	    JButton btnXoa= new JButton("Xóa");
-    	    btnXoa.setForeground(Color.WHITE);
-    	    btnXoa.setBackground(Color.red);
-    	    JButton btnLamMoi= new JButton("Làm mới");
-    	    btnLamMoi.setForeground(Color.WHITE);
-    	    btnLamMoi.setBackground(Color.green);
-    	    btn_QLS2.add(btnThem);
-    	    btn_QLS2.add(btnSua);
-    	    btn_QLS2.add(btnXoa);
-    	    btn_QLS2.add(btnLamMoi);
-    	    QLS2.add(btn_QLS2);
+    	     JPanel btn_QLS2 = new JPanel( new GridLayout(4,1,2,10));
+		
+    	     JButton btnThem= new JButton("Thêm");
+    	     btnThem.setForeground(Color.WHITE);
+    	     btnThem.setBackground(Color.CYAN);
+    	     JButton btnSua= new JButton("Sửa");
+    	     btnSua.setForeground(Color.WHITE);
+    	     btnSua.setBackground(Color.blue);
+    	     JButton btnXoa= new JButton("Xóa");
+    	     btnXoa.setForeground(Color.WHITE);
+    	     btnXoa.setBackground(Color.red);
+    	     JButton btnLamMoi= new JButton("Làm mới");
+    	     btnLamMoi.setForeground(Color.WHITE);
+    	     btnLamMoi.setBackground(Color.green);
+    	     btn_QLS2.add(btnThem);
+    	     btn_QLS2.add(btnSua);
+    	     btn_QLS2.add(btnXoa);
+    	     btn_QLS2.add(btnLamMoi);
+    	     QLS2.add(btn_QLS2);
+    	 
+    	     JPanel img_QLS2 = new JPanel();
+    	     img_QLS2.setBackground(Color.gray);
+    	     QLS2.add(img_QLS2);
+    	   
+    	     JPanel in_out_QLS2 = new JPanel( new GridLayout(2,1,2,10));
+    	     JButton inputQLS = new JButton("Nhập Excel");
+    	     JButton outputQLS = new  JButton("Xuất Excel");
+    	     in_out_QLS2.add(outputQLS);
+    	     in_out_QLS2.add(inputQLS);
+    	     QLS2.add(in_out_QLS2);
+    	   
+    	     JPanel QLS3 = new JPanel(new GridLayout(4, 6, 5, 5));
+    	     TitledBorder blackline = BorderFactory.createTitledBorder("Tìm kiếm");
+    	     blackline.setTitleJustification(TitledBorder.CENTER);
+    	     QLS3.setBorder(blackline);
+    	     JLabel maSach1 = new JLabel ("mã sách: ");
+    	     JLabel maNXB1 = new JLabel ("mã NXB: ");
+    	     JLabel maTheLoai1 = new JLabel ("mã thể loại: ");
+    	     JLabel maTacGia1 = new JLabel ("mã tác giả: ");
+    	     JLabel tenSach1 = new JLabel ("tên sách: ");
+    	     JLabel namXuatBan1 = new JLabel ("năm xuất bản: ");
+    	     JLabel khoangGia = new JLabel ("Khoảng giá: ");
+    	     JLabel khoangGiaTu = new JLabel("Từ: ");
+    	     JLabel khoangGiaDen = new JLabel("Đến: ");
+    	     JLabel namXBTu = new JLabel("NXB Từ: ");
+    	     JLabel namXBDen = new JLabel("NXB Đến: ");
+    	     JLabel space = new JLabel();
+    	    JButton btnTimKiem = new JButton("Tìm kiếm");
+    	     JLabel space2 = new JLabel();
+    	     JLabel space3 = new JLabel();
     	    
-    	    JPanel img_QLS2 = new JPanel();
-    	    img_QLS2.setBackground(Color.gray);
-    	    QLS2.add(img_QLS2);
-    	    
-    	    JPanel in_out_QLS2 = new JPanel( new GridLayout(2,1,2,10));
-    	    JButton inputQLS = new JButton("Nhập Excel");
-    	    JButton outputQLS = new  JButton("Xuất Excel");
-    	    in_out_QLS2.add(outputQLS);
-    	    in_out_QLS2.add(inputQLS);
-    	    QLS2.add(in_out_QLS2);
-    	    
-    	    JPanel QLS3 = new JPanel(new GridLayout(4, 6, 5, 5));
-    	    TitledBorder blackline = BorderFactory.createTitledBorder("Tìm kiếm");
-    	    blackline.setTitleJustification(TitledBorder.CENTER);
-    	    QLS3.setBorder(blackline);
-    	    JLabel maSach1 = new JLabel ("mã sách: ");
-    	    JLabel maNXB1 = new JLabel ("mã NXB: ");
-    	    JLabel maTheLoai1 = new JLabel ("mã thể loại: ");
-    	    JLabel maTacGia1 = new JLabel ("mã tác giả: ");
-    	    JLabel tenSach1 = new JLabel ("tên sách: ");
-    	    JLabel namXuatBan1 = new JLabel ("năm xuất bản: ");
-    	    JLabel khoangGia = new JLabel ("Khoảng giá: ");
-    	    JLabel khoangGiaTu = new JLabel("Từ: ");
-    	    JLabel khoangGiaDen = new JLabel("Đến: ");
-    	    JLabel namXBTu = new JLabel("NXB Từ: ");
-    	    JLabel namXBDen = new JLabel("NXB Đến: ");
-    	    JLabel space = new JLabel();
-    	    JLabel space1 = new JLabel();
-    	    JLabel space2 = new JLabel();
-    	    JLabel space3 = new JLabel();
-    	    
-    	    JTextField txtMaSach1 = new JTextField();
-    	    JTextField txtMaNXB1 = new JTextField();
-    	    JTextField txtMaTheLoai1 = new JTextField();
-    	    JTextField txtMaTacGia1 = new JTextField();
-    	    JTextField txtTenSach1 = new JTextField();
-    	    JTextField txtNamXuatBan1 = new JTextField();
-    	    JTextField txtKhoangGia = new JTextField();
-    	    JTextField txtKhoangGiaTu = new JTextField();
-    	    JTextField txtKhoangGiaDen = new JTextField();
-    	    JTextField txtNXBTu = new JTextField();
-    	    JTextField txtNXBDen = new JTextField();
-    	    QLS3.add(maSach1);
-    	    QLS3.add(txtMaSach1);
-    	    QLS3.add(maNXB1);
-    	    QLS3.add(txtMaNXB1);
-    	    QLS3.add(maTacGia1);
-    	    QLS3.add(txtMaTacGia1);
-    	    QLS3.add(maTheLoai1);
-    	    QLS3.add(txtMaTheLoai1);
-    	    QLS3.add(tenSach1);
-    	    QLS3.add(txtTenSach1);
-    	    QLS3.add(space);
-    	    QLS3.add(space1);
-    	    QLS3.add(khoangGia);
-   	        QLS3.add(space2);
-    	    QLS3.add(khoangGiaTu);
-    	    QLS3.add(txtKhoangGiaTu);
-    	    QLS3.add(khoangGiaDen);
-    	    QLS3.add(txtKhoangGiaDen);
-    	    QLS3.add(namXuatBan1);
-    	    QLS3.add(space3);
-    	    QLS3.add(namXBTu);
-    	    QLS3.add(txtNXBTu);
-    	    QLS3.add(namXBDen);
-    	    QLS3.add(txtNXBDen);
-    	    
-    	    
-    	    
+    	     JTextField txtMaSach1 = new JTextField();
+    	     JTextField txtMaNXB1 = new JTextField();
+    	     JTextField txtMaTheLoai1 = new JTextField();
+    	     JTextField txtMaTacGia1 = new JTextField();
+    	     JTextField txtTenSach1 = new JTextField();
+    	     JTextField txtKhoangGiaTu = new JTextField();
+    	     JTextField txtKhoangGiaDen = new JTextField();
+    	     JTextField txtNXBTu = new JTextField();
+    	     JTextField txtNXBDen = new JTextField();
+    	     QLS3.add(maSach1);
+    	     QLS3.add(txtMaSach1);
+			 QLS3.add(tenSach1);
+    	     QLS3.add(txtTenSach1);
+
+			 QLS3.add(space);
+    	     QLS3.add(btnTimKiem);
+
+    	     QLS3.add(maTheLoai1);
+    	     QLS3.add(txtMaTheLoai1);
+			 QLS3.add(space2);
+			 QLS3.add(khoangGia);
+    	     QLS3.add(space3);
+			 QLS3.add(namXuatBan1);
+			 QLS3.add(maNXB1);
+    	     QLS3.add(txtMaNXB1);
+    	     QLS3.add(khoangGiaTu);
+    	     QLS3.add(txtKhoangGiaTu);
+			 QLS3.add(namXBTu);
+    	     QLS3.add(txtNXBTu);
+			 QLS3.add(maTacGia1);
+    	     QLS3.add(txtMaTacGia1);
+    	     QLS3.add(khoangGiaDen);
+    	     QLS3.add(txtKhoangGiaDen);
+    	     QLS3.add(namXBDen);
+    	     QLS3.add(txtNXBDen);
     	    
     	    
+    	     String[][] data1 = {
+	        };
+	        String[] columnNames1 = { "Mã Sách", "Tên Sách", "Số Lượng", "Đơn Giá", "Giảm Giá", "Thành Tiền" };
+	       JTable j1 = new JTable(data1, columnNames1);
+	        JScrollPane sp2 = new JScrollPane(j1);
+    	    
+    	    JPanel QLS4 = new JPanel(new FlowLayout());
+			JPanel QLS4TK= new JPanel( new GridLayout(2,2));
+			JButton btnThongKe = new JButton("Thống Kê");
+			JLabel donGiaThapNhat = new JLabel("Đơn giá thấp nhất");
+			JLabel donGiaCaoNhat = new JLabel("Đơn giá cao nhất");
+			JLabel soLoaiSach = new JLabel("Số loại sách");
+			JLabel tongSoSach = new JLabel("Tổng số sách");
+			JTextField txtDonGiaThapNhat = new JTextField();
+			JTextField txtDonGiaCaoNhat = new JTextField();
+			JTextField txtSoLoaiSach = new JTextField();
+			JTextField txtTongSoSach = new JTextField();
+			QLS4.add(btnThongKe);
+			QLS4TK.add(donGiaThapNhat);
+			QLS4TK.add(txtDonGiaThapNhat);
+			QLS4TK.add(soLoaiSach);
+			QLS4TK.add(txtSoLoaiSach);
+			QLS4TK.add(donGiaCaoNhat);
+			QLS4TK.add(txtDonGiaCaoNhat);
+			QLS4TK.add(tongSoSach);
+			QLS4TK.add(txtTongSoSach);
+			QLS4.add(QLS4TK);
+			
+			// quanLySachCenter.setPreferredSize(new Dimension(600, 400)); 
+			// QLS2.setPreferredSize(new Dimension(600, 150));  
+			// QLS3.setPreferredSize(new Dimension(600, 100));
+			// sp2.setPreferredSize(new Dimension(600, 100));
+
     	    
     	    
-    	    quanLySach.add(QLS1);
-    	    quanLySach.add(QLS2);
-    	    quanLySach.add(QLS3);
-    	    quanLySachPanel.add(quanLySach);
+			quanLySachCenter.add(QLS2);
+			quanLySachCenter.add(QLS3);
+			quanLySachCenter.add(sp2);
+    	     
+    	     quanLySachPanel.add(quanLySachCenter,BorderLayout.CENTER);
+    	    quanLySachPanel.add(QLS4,BorderLayout.SOUTH);
     	    
     	    
     	    
